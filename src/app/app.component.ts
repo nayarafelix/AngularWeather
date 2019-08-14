6fd01494b0ede0fc4b6e7ab7fcc5c2b1b8ebbe9f
Logon Aluno <logonaluno@local.com>
2019-08-13 22:14:05 -0300

    App de consulta de temperatura por coordenada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'segundaAula';
+  latitude:String;
+  longitude:String;
+
+  ngOnInit() {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async position => {
+        this.latitude = `${position.coords.latitude}`;
+        this.longitude = `${position.coords.longitude}`;
+      });
+    }
+  }
+  
 }
---
+++ src/app/clima/clima.component.ts
@@ -0,0 +1,30 @@
+import { Input, Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-clima',
+  templateUrl: './clima.component.html',
+  styleUrls: ['./clima.component.scss']
+})
+export class ClimaComponent implements OnInit {
+  @Input() latitude:String;
+  @Input() longitude:String;
+  temperatura:String;
+  key:String = "6e24edd54ad31de7892c5aaaaaee8973";
+  url:String = "http://api.openweathermap.org/data/2.5/weather";
+  constructor(private http:HttpClient) { }
+
+  async ngOnInit() {
+    //alert(this.latitude);
+    //alert(this.longitude);
+  }
+
+  async getPositionData() {
+    this.url = `${this.url}?lat=${this.latitude}&lon=${this.longitude}&units=metric&APIKEY=${this.key}`;
+    var data = await this.http.get(`${this.url}`).toPromise();
+    this.temperatura = data.main.temp;
+    console.log(data)
+
+  }
+
+}
